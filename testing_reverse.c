2629d4835086d11d9af00fc64405dd332b213969
uwasepauline7 <puwase@andrew.cmu.edu>
2024-09-15 16:23:49 +0200

    dsiplaying the reversed and lowercase version of the orginal string

--- testing_reverse.c
+++ testing_reverse.c
@@ -4,10 +4,10 @@
 
 // Function to reverse a string and turn it into lower case
 void reverse_string(char *str) {
-    int length_of_string = strlen(str);
-    int left_side = 0, right_side = length_of_string - 1;
+    int length_of_string = strlen(str); 
+    int left_side = 0, right_side = length_of_string - 1; // variables for reversing
     
-    while (left_side < right_side) {
+    while (left_side < right_side) {                // set up the conditions for reversing and sametime changing into lower case
         char temp = tolower(str[left_side]);
         str[left_side] = tolower(str[right_side]);
         str[right_side] = temp;
@@ -23,9 +23,10 @@ void reverse_string(char *str) {
 int main() {
     char str[] = "Carnegie Mellon";
     
-    printf("Original string: %s\n", str);
+    printf("our orginal string: %s\n", str);
     reverse_string(str);
-    printf("Reversed and lowercase string: %s\n", str);
+    printf("Reversed and lower case version of the orginal string: %s\n", str);
+    printf("test passed");
     
     return 0;
 }
