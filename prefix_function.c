1bab5bd5ccd2cb8b169d13a87828983baedcc7e3
uwasepauline7 <puwase@andrew.cmu.edu>
2024-09-15 15:54:01 +0200

    creating the 2 variables to store our prefix

--- prefix_function.c
+++ prefix_function.c
@@ -15,19 +15,19 @@ bool is_prefix(const char *prefix, const char *str) {
 
 int main() {
     char str[] = "Carnegie Mellon";
-    char prefix1[] = "Carnegie";
-    char prefix2[] = "Mellon";
+    char prefix_1[] = "Carnegie";
+    char prefix_2[] = "Mellon";
     
-    if (is_prefix(prefix1, str)) {
-        printf("'%s' is a prefix of '%s'\n", prefix1, str);
+    if (is_prefix(prefix_1, str)) {
+        printf("'%s' is a prefix of '%s'\n", prefix_1, str);
     } else {
-        printf("'%s' is NOT a prefix of '%s'\n", prefix1, str);
+        printf("'%s' is NOT a prefix of '%s'\n", prefix_1, str);
     }
     
-    if (is_prefix(prefix2, str)) {
-        printf("'%s' is a prefix of '%s'\n", prefix2, str);
+    if (is_prefix(prefix_2, str)) {
+        printf("'%s' is a prefix of '%s'\n", prefix_2, str);
     } else {
-        printf("'%s' is NOT a prefix of '%s'\n", prefix2, str);
+        printf("'%s' is NOT a prefix of '%s'\n", prefix_2, str);
     }
     
     return 0;
