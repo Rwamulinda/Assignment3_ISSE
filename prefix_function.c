61d71c0886c5fcef02a81efe8f5606c20c07dfcc
uwasepauline7 <puwase@andrew.cmu.edu>
2024-09-15 15:58:34 +0200

    creating the if condition to check the prefix of our main string

--- prefix_function.c
+++ prefix_function.c
@@ -14,17 +14,17 @@ bool is_prefix(const char *prefix, const char *str) {
 }
 
 int main() {
-    char str[] = "Carnegie Mellon";
-    char prefix_1[] = "Carnegie";
+    char str[] = "Carnegie Mellon";    // declaring our string a
+    char prefix_1[] = "Carnegie";       // prefix variables
     char prefix_2[] = "Mellon";
     
     if (is_prefix(prefix_1, str)) {
-        printf("'%s' is a prefix of '%s'\n", prefix_1, str);
+        printf("'%s' is a prefix of '%s'\n", prefix_1, str);  // if condition to check if the prefix_1 is the real prefix of our main string
     } else {
-        printf("'%s' is NOT a prefix of '%s'\n", prefix_1, str);
+        printf("'%s' is NOT a prefix of '%s'\n", prefix_1, str);    // if it is not display this
     }
     
-    if (is_prefix(prefix_2, str)) {
+    if (is_prefix(prefix_2, str)) {                                // check our second string Melllon if it is a prefix of our main string
         printf("'%s' is a prefix of '%s'\n", prefix_2, str);
     } else {
         printf("'%s' is NOT a prefix of '%s'\n", prefix_2, str);
